a7567255e0e8d733e15d915a7de125d4f3d87183
slavonicblin <78567665+slavonicblin@users.noreply.github.com>
2021-12-10 18:37:27 -0600

    Add project files.

---
+++ HealthCatchGavinL/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StopToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DiffucultyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EasyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ModerateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HardToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InsaneToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutGameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProgrammerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.tmrKitMove = New System.Windows.Forms.Timer(Me.components)
+        Me.picKit = New System.Windows.Forms.PictureBox()
+        Me.lblTimeRemaining = New System.Windows.Forms.Label()
+        Me.tmrTimeLimit = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.picKit, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GameToolStripMenuItem, Me.DiffucultyToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(754, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'GameToolStripMenuItem
+        '
+        Me.GameToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.StopToolStripMenuItem})
+        Me.GameToolStripMenuItem.Name = "GameToolStripMenuItem"
+        Me.GameToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.GameToolStripMenuItem.Text = "Game"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'StopToolStripMenuItem
+        '
+        Me.StopToolStripMenuItem.Name = "StopToolStripMenuItem"
+        Me.StopToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.StopToolStripMenuItem.Text = "Stop"
+        '
+        'DiffucultyToolStripMenuItem
+        '
+        Me.DiffucultyToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EasyToolStripMenuItem, Me.ModerateToolStripMenuItem, Me.HardToolStripMenuItem, Me.InsaneToolStripMenuItem})
+        Me.DiffucultyToolStripMenuItem.Name = "DiffucultyToolStripMenuItem"
+        Me.DiffucultyToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.DiffucultyToolStripMenuItem.Text = "Difficulty"
+        '
+        'EasyToolStripMenuItem
+        '
+        Me.EasyToolStripMenuItem.Checked = True
+        Me.EasyToolStripMenuItem.CheckOnClick = True
+        Me.EasyToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.EasyToolStripMenuItem.Name = "EasyToolStripMenuItem"
+        Me.EasyToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.EasyToolStripMenuItem.Text = "Easy"
+        '
+        'ModerateToolStripMenuItem
+        '
+        Me.ModerateToolStripMenuItem.CheckOnClick = True
+        Me.ModerateToolStripMenuItem.Name = "ModerateToolStripMenuItem"
+        Me.ModerateToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.ModerateToolStripMenuItem.Text = "Moderate"
+        '
+        'HardToolStripMenuItem
+        '
+        Me.HardToolStripMenuItem.CheckOnClick = True
+        Me.HardToolStripMenuItem.Name = "HardToolStripMenuItem"
+        Me.HardToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.HardToolStripMenuItem.Text = "Hard"
+        '
+        'InsaneToolStripMenuItem
+        '
+        Me.InsaneToolStripMenuItem.CheckOnClick = True
+        Me.InsaneToolStripMenuItem.Name = "InsaneToolStripMenuItem"
+        Me.InsaneToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.InsaneToolStripMenuItem.Text = "Insane"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutGameToolStripMenuItem, Me.ProgrammerToolStripMenuItem})
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'AboutGameToolStripMenuItem
+        '
+        Me.AboutGameToolStripMenuItem.Name = "AboutGameToolStripMenuItem"
+        Me.AboutGameToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.AboutGameToolStripMenuItem.Text = "Game instructions"
+        '
+        'ProgrammerToolStripMenuItem
+        '
+        Me.ProgrammerToolStripMenuItem.Name = "ProgrammerToolStripMenuItem"
+        Me.ProgrammerToolStripMenuItem.Size = New System.Drawing.Size(170, 22)
+        Me.ProgrammerToolStripMenuItem.Text = "Programmer"
+        '
+        'lblScore
+        '
+        Me.lblScore.BackColor = System.Drawing.Color.FromArgb(CType(CType(86, Byte), Integer), CType(CType(133, Byte), Integer), CType(CType(162, Byte), Integer))
+        Me.lblScore.Font = New System.Drawing.Font("Verdana", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(12, 34)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(83, 65)
+        Me.lblScore.TabIndex = 2
+        Me.lblScore.Text = "0"
+        Me.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tmrKitMove
+        '
+        '
+        'picKit
+        '
+        Me.picKit.Image = Global.HealthCatchGavinL.My.Resources.Resources.smallkit
+        Me.picKit.Location = New System.Drawing.Point(314, 209)
+        Me.picKit.Name = "picKit"
+        Me.picKit.Size = New System.Drawing.Size(69, 90)
+        Me.picKit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picKit.TabIndex = 1
+        Me.picKit.TabStop = False
+        '
+        'lblTimeRemaining
+        '
+        Me.lblTimeRemaining.BackColor = System.Drawing.Color.FromArgb(CType(CType(86, Byte), Integer), CType(CType(133, Byte), Integer), CType(CType(162, Byte), Integer))
+        Me.lblTimeRemaining.Font = New System.Drawing.Font("Verdana", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimeRemaining.Location = New System.Drawing.Point(659, 34)
+        Me.lblTimeRemaining.Name = "lblTimeRemaining"
+        Me.lblTimeRemaining.Size = New System.Drawing.Size(83, 65)
+        Me.lblTimeRemaining.TabIndex = 3
+        Me.lblTimeRemaining.Text = "0"
+        Me.lblTimeRemaining.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tmrTimeLimit
+        '
+        Me.tmrTimeLimit.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(184, Byte), Integer), CType(CType(56, Byte), Integer), CType(CType(59, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(754, 500)
+        Me.Controls.Add(Me.lblTimeRemaining)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.picKit)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Health Kit Catch"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.picKit, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents GameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StopToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DiffucultyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EasyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ModerateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HardToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InsaneToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutGameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProgrammerToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents picKit As PictureBox
+    Friend WithEvents lblScore As Label
+    Friend WithEvents tmrKitMove As Timer
+    Friend WithEvents lblTimeRemaining As Label
+    Friend WithEvents tmrTimeLimit As Timer
+End Class
---
+++ HealthCatchGavinL/Form1.vb
@@ -0,0 +1,207 @@
+﻿'Health Kit Catch application
+'G. Lepischak
+'Dec. 07 2021
+'The user trys to click picure while it moves
+'extras:
+'variable score increase and time limit based on difficulty
+'audio on game complete
+Public Class Form1
+    Private Const EASY_LEVEL As Integer = 2500
+    Private Const MODERATE_LEVEL As Integer = 1500
+    Private Const HARD_LEVEL As Integer = 600
+    Private Const INSANE_LEVEL As Integer = 150
+    Private Const EASY_SCORE As Integer = 2
+    Private Const MODERATE_SCORE As Integer = 4
+    Private Const HARD_SCORE As Integer = 8
+    Private Const INSANE_SCORE As Integer = 16
+    Private Const EASY_TIME As Integer = 60
+    Private Const MODERATE_TIME As Integer = 40
+    Private Const HARD_TIME As Integer = 30
+    Private Const INSANE_TIME As Integer = 20
+
+    Private moveInterval As Integer
+    Private score As Integer
+    Private scoreIncrease As Integer
+    Private gameOn As Boolean = False
+    Private timelimit As Integer
+    Private bonusOn As Boolean = False
+    Private Sub EasyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EasyToolStripMenuItem.Click
+        EasyToolStripMenuItem.Checked = True
+        ModerateToolStripMenuItem.Checked = False
+        HardToolStripMenuItem.Checked = False
+        InsaneToolStripMenuItem.Checked = False
+
+        moveInterval = EASY_LEVEL
+        scoreIncrease = EASY_SCORE
+        timelimit = EASY_TIME
+        lblTimeRemaining.Text = timelimit
+    End Sub
+
+    Private Sub ModerateToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ModerateToolStripMenuItem.Click
+        EasyToolStripMenuItem.Checked = False
+        ModerateToolStripMenuItem.Checked = True
+        HardToolStripMenuItem.Checked = False
+        InsaneToolStripMenuItem.Checked = False
+
+        moveInterval = MODERATE_LEVEL
+        scoreIncrease = MODERATE_SCORE
+        timelimit = MODERATE_TIME
+        lblTimeRemaining.Text = timelimit
+    End Sub
+
+    Private Sub HardToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HardToolStripMenuItem.Click
+        EasyToolStripMenuItem.Checked = False
+        ModerateToolStripMenuItem.Checked = False
+        HardToolStripMenuItem.Checked = True
+        InsaneToolStripMenuItem.Checked = False
+
+        moveInterval = HARD_LEVEL
+        scoreIncrease = HARD_SCORE
+        timelimit = HARD_TIME
+        lblTimeRemaining.Text = timelimit
+    End Sub
+
+    Private Sub InsaneToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InsaneToolStripMenuItem.Click
+        EasyToolStripMenuItem.Checked = False
+        ModerateToolStripMenuItem.Checked = False
+        HardToolStripMenuItem.Checked = False
+        InsaneToolStripMenuItem.Checked = True
+
+        moveInterval = INSANE_LEVEL
+        scoreIncrease = INSANE_SCORE
+        timelimit = INSANE_TIME
+        lblTimeRemaining.Text = timelimit
+    End Sub
+
+    Private Sub picKit_Click(sender As Object, e As EventArgs) Handles picKit.Click
+        If gameOn = True Then
+            My.Computer.Audio.Play(My.Resources.pickup,
+                AudioPlayMode.Background)
+            score = score + scoreIncrease
+            lblScore.Text = score
+        End If
+    End Sub
+
+    Private Sub StartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartToolStripMenuItem.Click
+        If EasyToolStripMenuItem.Checked Then
+            moveInterval = EASY_LEVEL
+            scoreIncrease = EASY_SCORE
+            timelimit = EASY_TIME
+            lblTimeRemaining.Text = timelimit
+        End If
+        gameOn = True
+        tmrKitMove.Enabled = True
+        picKit.Enabled = True
+        tmrKitMove.Interval = moveInterval
+        tmrTimeLimit.Enabled = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        gameOn = False
+        picKit.Enabled = False
+        tmrKitMove.Enabled = False
+        tmrTimeLimit.Enabled = False
+    End Sub
+
+    Private Sub tmrKitMove_Tick(sender As Object, e As EventArgs) Handles tmrKitMove.Tick
+        Randomize()
+        Const MAX_TOP_POSTION As Integer = 398
+        Const MAX_LEFT_POSTION As Integer = 348
+        Const LOW_POSTION As Integer = 105
+        Dim randomTop As Integer
+        Dim randomLeft As Integer
+        Static counter As Integer = 0
+
+        If timelimit = 0 Then
+            gameOn = False
+            tmrKitMove.Enabled = False
+            tmrTimeLimit.Enabled = False
+            My.Computer.Audio.Play(My.Resources.gameover,
+                AudioPlayMode.Background)
+            MessageBox.Show("Game over. please try again.", "Game over", MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation)
+            Application.Exit()
+        End If
+
+        If score >= 100 Then
+            gameOn = False
+            tmrKitMove.Enabled = False
+            tmrTimeLimit.Enabled = False
+            My.Computer.Audio.Play(My.Resources._07_Red_Triumphs_,
+                AudioPlayMode.Background)
+            MessageBox.Show("Congratulations you made it to 100", "Game complete", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+            Application.Exit()
+        End If
+
+        randomTop = Int((MAX_TOP_POSTION - LOW_POSTION + 1) * Rnd() + LOW_POSTION)
+        picKit.Top = randomTop
+        randomLeft = Int((MAX_LEFT_POSTION - LOW_POSTION + 1) * Rnd() + LOW_POSTION)
+        picKit.Left = randomLeft
+
+        counter = counter + 1
+    End Sub
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+        If gameOn = True Then
+            score = score - 1
+            lblScore.Text = score
+            My.Computer.Audio.Play(My.Resources.Roblox_Death_Sound,
+                AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub tmrTimeLimit_Tick(sender As Object, e As EventArgs) Handles tmrTimeLimit.Tick
+        Randomize()
+        Const MAX_BONUS_CHANCE = 5
+        Const MIN_BONUS_CHANCE = 1
+        Static bonusCounter As Integer
+        Static bonusActivator As Integer
+        Static scoreIncreased As Boolean = False
+        If gameOn = True Then
+            timelimit = timelimit - 1
+            lblTimeRemaining.Text = timelimit
+        End If
+        If bonusOn = False Then
+            bonusActivator = Int((MAX_BONUS_CHANCE - MIN_BONUS_CHANCE + 1) * Rnd() + MIN_BONUS_CHANCE)
+        End If
+        Select Case bonusActivator
+            Case 2
+                bonusOn = True
+        End Select
+        If bonusOn = True Then
+            bonusCounter = bonusCounter + 1
+        End If
+        If bonusOn = True And scoreIncreased = False Then
+            scoreIncrease = scoreIncrease + 5
+            scoreIncreased = True
+        End If
+        If bonusCounter = 5 Then
+            bonusOn = False
+            scoreIncrease = scoreIncrease - 5
+            bonusCounter = 0
+        End If
+    End Sub
+
+    Private Sub AboutGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutGameToolStripMenuItem.Click
+        MessageBox.Show("Object of the game: get to 100 points (shown on left) before the time (shown on right) runs out" & vbCrLf &
+                        "Step 1: select your difficulty with the difficulty tab" & vbCrLf &
+                        "Easy mode: movement every 2.5 seconds, 2 points per click, 60 second time limit" & vbCrLf &
+                        "Moderate mode: movement every 1.5 seconds, 4 points per click, 40 second time limit" _
+                        & vbCrLf & "Hard mode: movement every .6 seconds, 8 points per click, 30 second time limit" _
+                        & vbCrLf & "Insane mode: movement every .15 seconds, 16 points per click, 20 second time limit" & vbCrLf &
+                        "Step 2: click the game tab then start to start" & vbCrLf &
+                        "Step 3: click the health kit to get points" & vbCrLf &
+                        "Every second there is a chance to get a score increase bonus of 5 for 5 seconds",
+                        "Game instructions", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ProgrammerToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProgrammerToolStripMenuItem.Click
+        MessageBox.Show("Programmed by Gavin Lepischak on Dec. 09 2021", "Program info", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub StopToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StopToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+End Class
